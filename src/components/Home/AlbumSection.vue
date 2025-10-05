5f2fa19001eeb1010142a0d60c7080381c355073
haulv1529 <lvhau1529@gmail.com>
2025-10-06 02:44:22 +0700

    Update package.json to include @types/node, modify tsconfig.node.json to add types for node and improve formatting, and enhance AlbumSection.vue by adding dynamic IDs to album divs.

--- src/components/Home/AlbumSection.vue
+++ src/components/Home/AlbumSection.vue
@@ -45,7 +45,7 @@
 
     <div class="grid grid-cols-2 gap-2">
       <template v-for="i in 4" :key="i">
-        <div class="h-64">
+        <div :id="`album-${i}`" class="h-64">
           <img
             class="w-full h-full object-cover"
             src="https://images.unsplash.com/photo-1606800052052-a08af7148866?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
